d214b369b8a840e4b5b26117112c473ecb9957f2
Felix <felix.signup@gmail.com>
2017-08-07 19:27:07 +0800

    asdsa

--- addBinary.js
+++ addBinary.js
@@ -1,3 +1,16 @@
+/*
+Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after the addition.
+
+The binary number returned should be a string.
+*/
 function addBinary(a,b) {
-  console.log((a+b).toString(2));
+  var res=a+b;
+  var r="";
+  while(res>1){
+    r=res%2+r;
+    res=(res-res%2)/2;
+  }
+  r=res+r;
+
+  console.log(r);
 }
